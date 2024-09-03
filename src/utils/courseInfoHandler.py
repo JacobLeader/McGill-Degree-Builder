a6ab2a839c55ee34b4b7ce92e45ada27123ff762
Jacob <Jacob.leader06@gmail.com>
2024-09-03 00:40:52 -0400

    feat: getCourseInfo(), getCourseLevel()

---
+++ src/utils/courseInfoHandler.py
@@ -0,0 +1,15 @@
+import re
+from utils.courseLoader import loadCourseData
+
+# class CourseInfo():
+#     def __init__(self):
+courses = loadCourseData()
+    
+def getCourseInfo(course_title):
+    return courses[course_title]
+    
+def getCourseLevel(course_code):
+    match = re.search(r'\d+$', course_code) # \d+ means more then one digit, $ means its at the end of the string
+    if match:
+        return match.group(0)
+    
