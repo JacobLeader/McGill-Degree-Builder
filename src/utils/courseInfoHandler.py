605025c748a780e22d0282b0fe80ed052f3b353c
Jacob <Jacob.leader06@gmail.com>
2024-09-03 15:00:22 -0400

    feat: get info of a major, extract course title

--- src/utils/courseInfoHandler.py
+++ src/utils/courseInfoHandler.py
@@ -1,5 +1,6 @@
 import re
 from utils.courseLoader import loadCourseData
+import json
 
 # class CourseInfo():
 #     def __init__(self):
@@ -13,3 +14,21 @@ def getCourseCode(course_code):
     if match:
         return match.group(0)
     
+def extractCourseTitle(text):
+    # "COMP 251 Algorithms and Data Structures (3 credits)" -> "Algorithms and Data Structures"
+    match = re.search(r'\d+\s(.*?)\s\(', text)
+    if match:
+        return match.group(1).strip()
+    return None
+    
+def getMajorInfo(major):
+    file_name_ending = major.replace(" ", "_")
+    file_path = f"data/major_{file_name_ending}.json"
+    
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
+    
+def isSimilarCourse(course1: str, course2: str):
+    # TODO: ex. deductive logic 1 vs deductive logic 2
+    ...
