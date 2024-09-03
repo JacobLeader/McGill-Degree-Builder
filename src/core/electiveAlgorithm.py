e0b4c221735f14a461650bb5809de6e072957efb
Jacob <Jacob.leader06@gmail.com>
2024-09-03 01:09:21 -0400

    feat: recommend elective by preferred department

--- src/core/electiveAlgorithm.py
+++ src/core/electiveAlgorithm.py
@@ -1,11 +1,8 @@
 
 import sys
- 
-# append the path of the
-# parent directory
-sys.path.append("..")
-
-from utils.courseInfoHandler import getCourseLevel, getCourseInfo
+sys.path.append("..") # append the path of the parent directory to allow access to sibling directories
+from utils.courseInfoHandler import getCourseCode, getCourseInfo
+from utils.courseLoader import loadCourseData
 
 class ElectiveRecomender():
     def __init__(self, courses, liked_courses, disliked_courses, completed_courses, preferred_departments):
@@ -22,9 +19,29 @@ class ElectiveRecomender():
         # If there is a user says they want to do an elective, they haven't done it yet, and the level is within a year, it seems straight forward to suggest it
         for course in self.liked_courses:
             course_info = getCourseInfo(course)
-            course_level = int(getCourseLevel(course_info['course code'])[0]) #  1,2,3,4,5 or 6
+            course_level = int(getCourseCode(course_info['course code'])[0]) #  1,2,3,4,5 or 6
             if course not in self.completed_courses and abs(year - course_level) <= 1:
                 return course
             
+        courses_data = loadCourseData()
+        for course_title in courses_data.keys():
+            try:
+                course_info = courses_data[course_title]
+                valid_course = True
+                course_level = int(getCourseCode(course_info['course code'])[0]) #  1,2,3,4,5 or 6
+                
+                for course in self.completed_courses + self.disliked_courses: # check if the course is in either completed or disliked courses
+                    #! more logic is needed here to avoid recommending similar courses to those that are disliked, ex. deductive logic 1 & deductive logic 2
+                    course_code = getCourseInfo(course)['course code']
+                    if course_code == course_info['course code']:
+                        valid_course = False
+                        continue
+                    
+                if valid_course and course_info['dept'] in self.preferred_departments and abs(year - course_level) <= 1:
+                    # print(course_info['dept'])
+                    return course_title
+            except:
+                pass
+                
 
             
